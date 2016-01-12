f96091cb339dc192d5dadf449bfa053f7ade0e0c
Romain SOMMERARD <rsommerard@gmail.com>
2016-01-12 08:58:05 +0100

    Add logs formatting

--- script.sh
+++ script.sh
@@ -11,10 +11,18 @@ git clone https://github.com/rsommerard/program-repair-test.git
 cd program-repair-test/
 mvn clean test 1> ../logs/current-test.log
 
+line=`grep -m 1 -n "^-" ../logs/current-test.log | cut -f 1 -d :`
+tail -n +$line ../logs/current-test.log > ../logs/current-test.log.tmp
+mv ../logs/current-test.log.tmp ../logs/current-test.log
+
 mvn clean
 
 git checkout HEAD~1
 mvn clean test 1> ../logs/previous-test.log
+
+line=`grep -m 1 -n "^-" ../logs/previous-test.log | cut -f 1 -d :`
+tail -n +$line ../logs/previous-test.log > ../logs/previous-test.log.tmp
+mv ../logs/previous-test.log.tmp ../logs/previous-test.log
 
 git checkout master
 
@@ -31,3 +39,7 @@ git checkout master
 
 
 mvn clean test 1> ../logs/after-test.log
+
+line=`grep -m 1 -n "^-" ../logs/after-test.log | cut -f 1 -d :`
+tail -n +$line ../logs/after-test.log > ../logs/after-test.log.tmp
+mv ../logs/after-test.log.tmp ../logs/after-test.log
