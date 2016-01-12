6b111b3d7e7f7a1cb0d4c45cf58eb8fda7a01281
VaubanParty <yassine.badache@gmail.com>
2016-01-12 08:28:57 +0100

    Real initial commit as a Maven project

---
+++ src/main/java/proc/ProcessorGitTaker.java
@@ -0,0 +1,10 @@
+package proc;
+
+public class ProcessorGitTaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/proc/ProcessorGitTakerTest.java
@@ -0,0 +1,14 @@
+package proc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProcessorGitTakerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
